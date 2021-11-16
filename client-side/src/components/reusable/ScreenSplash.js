b8889828fc83e1a34466c279820e1b8ffcf8ec00
A. Drakiel <ismael.esquinca@ltte.com.mx>
2021-11-16 07:30:33 -0600

    [NEW] .- Auto redirect from splash

--- client-side/src/components/reusable/ScreenSplash.js
+++ client-side/src/components/reusable/ScreenSplash.js
@@ -2,17 +2,27 @@ import { useEffect, useState } from 'react'
 import { debug } from '../../utilities/helper'
 import pc from '../../../package.json'
 import logo from '../../assets/img/logo.jpg'
+import { useNavigate } from "react-router-dom"
 
 const ScreenSplash = () => {
     const [packVersion, setPackVersion] = useState('')
+    const navigate = useNavigate()
 
     useEffect(() => {
         debug('nerv')
         setPackVersion(`${pc.description} - ${pc.version}`)
+        setTimeout(() => {
+            goCandidates()
+        }, 3000);
     }, [])
 
+    const goCandidates = () => {
+        navigate('/candidates')
+    }
+
     return (
-        <div className="d-flex flex-column min-vh-100 justify-content-center align-items-center">
+        <div className="d-flex flex-column min-vh-100 justify-content-center align-items-center"
+            onClick={goCandidates} >
             <img src={logo} alt="splash" className="img-fluid logo_middle" />
             <p>{packVersion}</p>
         </div>
